e3bb538ba6e7340bd8f074128e85fd14c04c9f35
gabrielrmodesto <gabrielrmodesto5@gmail.com>
2018-06-07 07:56:02 -0300

    Alterar, buscar e remover adicionados

--- app.py
+++ app.py
@@ -14,11 +14,28 @@ def remover(nomes):
 	nome = raw_input()
 	nomes.remove(nome)
 
+def alterar(nomes):
+	print 'Qual o nome deseja alterar?'
+	nome_alterar = raw_input()
+	if(nome_alterar in nomes):
+		posicao = nomes.index(nome_alterar)
+		print 'Novo nome: '
+		novo_nome = raw_input()
+		nomes[posicao] = novo_nome
+	
+def procurar(nomes):
+	print 'Qual nome voce quer procurar?'
+	busca = raw_input()
+	if(busca in nomes):
+		print 'O nome %s esta cadastrado' % busca
+	else:
+		print 'O nome %s nao existe' % busca
+
 def menu():
 	nomes = []
 	escolha = ''
 	while (escolha != '0'):
-		print 'Digite:\n 1 para cadastrar\n 2 para listar\n 3 para remover \n0 para encerrar'
+		print 'Digite:\n 1 para cadastrar\n 2 para listar\n 3 para remover\n 4 para alterar \n 5 para procurar\n 0 para encerrar'
 		escolha = raw_input()
 		if(escolha == '1'):
 			cadastrar(nomes)
@@ -26,5 +43,9 @@ def menu():
 			listar(nomes)
 		if(escolha == '3'):
 			remover(nomes)
+		if(escolha == '4'):
+			alterar(nomes)
+		if(escolha == '5'):
+			procurar(nomes)
 
 menu()
