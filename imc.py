2d366f980877819f72bcb8d82872860ff8a36045
gabrielrmodesto <gabrielrmodesto5@gmail.com>
2018-06-12 19:54:42 -0300

    Classe para calculo do imc

---
+++ imc.py
@@ -0,0 +1,7 @@
+# Objetivo é criar uma classe Pessoa. Uma pessoa possui um nome, peso (em kilo) 
+# e altura (em metro). Uma vez criado uma pessoa, podemos imprimir o valor do IMC:
+
+# >>> from models import Pessoa
+# >>> pessoa = Pessoa("Ronaldo", 105, 1.78)
+# >>> pessoa.imprime()
+# Imc de Ronaldo: 33.1397550814
