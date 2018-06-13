80634407368e74e152ec059b0bc35ab18a4510bd
gabrielrmodesto <gabrielrmodesto5@gmail.com>
2018-06-13 12:03:07 -0300

    Metodo de creditos e tipo implementado

--- models.py
+++ models.py
@@ -2,11 +2,12 @@
 
 class Perfil(object):
 	'Classe padrao para perfis de usuario'
-	def __init__(self, nome, telefone, empresa):
+	def __init__(self, nome, telefone, empresa,tipo):
 		self.nome = nome
 		self.telefone = telefone
 		self.empresa = empresa
 		self.__curtidas = 0
+		self.__tipo = tipo
 
 	def imprimir(self):
 		print "Nome: %s\nTelefone: %s\nEmpresa: %s\nCurtidas: %s" % (self.nome,self.telefone,self.empresa,self.__curtidas)
@@ -16,3 +17,9 @@ class Perfil(object):
 
 	def obter_curtidas(self):
 		return self.__curtidas
+
+	def obter_creditos(self):
+		return self.__curtidas * 10.0
+
+	def obter_tipo(self):
+		return self.__tipo
