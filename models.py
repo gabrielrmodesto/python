82223b8c34dede322b69bc751f845cce43440d69
gabrielrmodesto <gabrielrmodesto5@gmail.com>
2018-06-13 19:51:27 -0300

    Classe de perfil vip adicionado, conceito de heranca

--- models.py
+++ models.py
@@ -2,12 +2,11 @@
 
 class Perfil(object):
 	'Classe padrao para perfis de usuario'
-	def __init__(self, nome, telefone, empresa,tipo):
+	def __init__(self, nome, telefone, empresa):
 		self.nome = nome
 		self.telefone = telefone
 		self.empresa = empresa
 		self.__curtidas = 0
-		self.__tipo = tipo
 
 	def imprimir(self):
 		print "Nome: %s\nTelefone: %s\nEmpresa: %s\nCurtidas: %s" % (self.nome,self.telefone,self.empresa,self.__curtidas)
@@ -18,8 +17,11 @@ class Perfil(object):
 	def obter_curtidas(self):
 		return self.__curtidas
 
-	def obter_creditos(self):
-		return self.__curtidas * 10.0
+class Perfil_Vip(Perfil):
+	'Classe padrao para perfis de usuario'
+	def __init__(self,nome,telefone,empresa,apelido):
+		super(Perfil_Vip, self).__init__(nome,telefone,empresa)
+		self.apelido = apelido
 
-	def obter_tipo(self):
-		return self.__tipo
+	def obter_creditos(self):
+		return super(Perfil_Vip, self).obter_curtidas() * 10.0
